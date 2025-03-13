704f8d4b2bd53dd57082aaf43fba6344e5a7a69a
annamartin22 <annamartinanna100@gmail.com>
2025-03-13 11:22:42 +0530

    this is positivenegative

---
+++ Positivenegative/PositiveNegative.java
@@ -0,0 +1,13 @@
+public class PositiveNegative{
+    public static void main(String args[]){
+        int x=35;
+        if(x>=0)
+        {
+            System.out.println("positive");
+
+        }
+        else
+        System.out.println("negative");
+    }        
+        
+    }

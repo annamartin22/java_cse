8d6e4556e76f0075b4c9c65719d494ef3b169a6f
annamartin22 <annamartinanna100@gmail.com>
2025-03-13 11:14:38 +0530

    this is majorminnerfinder

---
+++ Major minner Finder/MajorMinnerFinder.java
@@ -0,0 +1,13 @@
+public class MajorMinnerFinder{
+    public static void main(String args[]){
+        int y=18;
+        if(y>=18)
+        {
+            System.out.println("Major");
+
+        }
+        else
+        System.out.println("Minner");
+    }        
+        
+    }

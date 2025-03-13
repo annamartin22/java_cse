191381d4aab6439c46cd4f92d82973e04ae06e9a
annamartin22 <annamartinanna100@gmail.com>
2025-03-13 11:05:49 +0530

    this is IfElsestatment

---
+++ IfElse/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse{
+    public static void main(String args[]){
+        if(5>3)
+        {
+            System.out.println(true);
+
+        }
+        else
+        System.out.println(false);
+        
+    }
+}

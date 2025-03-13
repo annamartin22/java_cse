3b49e7aa45827f66d17786199ef1fd045918bcf5
annamartin22 <annamartinanna100@gmail.com>
2025-03-13 11:37:28 +0530

    this is gender

---
+++ gender/Gender.java
@@ -0,0 +1,15 @@
+public class Gender{
+    public static void main(String args[]){
+        char gender='m';
+        if(gender=='m')
+        {
+            System.out.println("it is Male");
+
+        }
+        else
+        {
+            System.out.println("it is female");
+    }
+    }        
+        
+    }

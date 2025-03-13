60b84fb3b8c62061a6b5c58d28d2d08815df6ab9
annamartin22 <annamartinanna100@gmail.com>
2025-03-13 10:48:15 +0530

    this is conditionalstatment

---
+++ ConditionalStatement/ConditionalStatement.java
@@ -0,0 +1,11 @@
+public class ConditionalStatement{
+    public static void main(String args[]){
+        if(5>2){
+            System.out.println(true);
+        }
+        if(5<2){
+            System.out.println(false);
+            
+        }
+    }
+}

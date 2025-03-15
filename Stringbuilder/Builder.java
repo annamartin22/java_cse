e46490406d4460f9523002baf05a8969dd316feb
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 10:50:32 +0530

    this is string builder

---
+++ Stringbuilder/Builder.java
@@ -0,0 +1,9 @@
+public class Builder{
+    public static void main(String args[]){
+        String s1="anna";
+        String s2="martin";
+        System.out.println("mae:"+(s1+s2));
+        
+        
+    }
+}

c4d2e425b71e43bdcd65335478d3304a5ae94a75
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 11:02:38 +0530

    this is leapuyear

---
+++ LeapYear/Leapyear.java
@@ -0,0 +1,15 @@
+
+public class Leapyear{
+    public static void main(String args[]){
+       int i=2020;
+       if(i%4==0)
+       {
+        System.out.println("Leapyear");
+       }
+       else{
+        System.out.println("not a leapyear");
+       }
+    }
+}
+
+    

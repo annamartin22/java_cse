29f4d19345670c21cc1d11b6f4f352f95cff8143
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 15:11:31 +0530

    this is nextint age

---
+++ nextint/Nextint.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Nextint
+{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("age");
+        int age=input.nextInt();
+            System.out.println(age);
+    }
+}

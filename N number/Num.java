8e8ec905fc14340981c2faacc8bc564fd94cc2fd
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 15:20:57 +0530

    this is n numberss

---
+++ N number/Num.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Num{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter the numbers");
+        int num=input.nextInt();
+        int i=1;
+        while(i<=num)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
+
+        

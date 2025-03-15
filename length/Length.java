b89dde2346314ba3cb60825fe9b6b6a25c1b891f
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 11:10:11 +0530

    this is length

---
+++ length/Length.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Length{
+    public static void main(String args[]){
+        String name="hello world";
+        
+        System.out.println(name.length());
+        
+        
+    }
+}

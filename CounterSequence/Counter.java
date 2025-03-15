200976fe3e77887499a35369908c95e2f926612e
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 10:24:55 +0530

    this is Countersequence

---
+++ CounterSequence/Counter.java
@@ -0,0 +1,11 @@
+public class Counter{
+    public static void main(String args[]){
+        for(int i=2020;i<2030;i++)
+        {
+            if(i%4==0){
+
+            }
+            System.out.println(i);
+        }
+    }
+}

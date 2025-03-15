7deadad494f7a7656cd61e1a635ecd9f705e3987
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 10:06:13 +0530

    this is Seventh table

---
+++ Seventhtable/Table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Table{
+    public static void main(String args[]){
+        
+       
+        int i=0;
+        int x=0;
+        while(i<70){
+            i=i+7;
+            x=x+1;
+            System.out.println(x+"x7="+i);
+            
+        }
+        
+    }
+}

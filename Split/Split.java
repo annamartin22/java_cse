dcb21c84bdd2a9a7912fa185fccbaa98fd589deb
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 10:19:23 +0530

    this is split

---
+++ Split/Split.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Split{
+    public static void main(String args[]){
+        String name="hello world";
+        String names[]=name.split(",");
+        System.out.println("names[0]");
+        
+        
+    }
+}

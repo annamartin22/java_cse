ba1fc488120a61196b1097e7eff27ae61008eddc
annamartin22 <annamartinanna100@gmail.com>
2025-03-15 10:14:10 +0530

    this is simpleinheritance

---
+++ Simpleinheritance/Simple.java
@@ -0,0 +1,18 @@
+class parent{
+    public void add(){
+        System.out.println("hello world");
+    }
+}
+class Child extends parent{
+    void display(){
+        add();
+    }
+}
+public class Simple{
+    public static void main(String args[]){
+        Child obj=new Child();
+        obj.display();
+    }
+}
+    
+
